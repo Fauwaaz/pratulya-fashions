68a8ad28d44e739ddb37d0ab863d5edb1c400fe4
Fauwaaz <fauwaazshaikh999@gmail.com>
2024-10-26 17:42:02 +0530

    Update: Contact form

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -14,38 +14,38 @@ const Contact = () => {
         <div className="container mx-auto px-4 py-10">
           <div className="flex flex-col lg:flex-row gap-10">
 
-            {/* Left Column - Contact Information */}
             <div className="lg:w-1/2">
-              <h2 className="text-3xl font-bold mb-4">Contact Information</h2>
+              <h2 className="text-3xl font-bold mb-4">Contact Pratulya Fashions - We're Here to Help
+              </h2>
               <p className="mb-8 text-gray-600 leading-5">
-                We'd love to hear from you! Feel free to reach out with any questions, comments, or inquiries.
+                Thank you for your interest in Pratulya Fashions, Mumbai's trusted provider of apparel manufacturing subcontractor services. Whether you are looking for bulk T-shirts, custom corporate uniforms, or specialized clothing solutions for men and women, we are here to assist you.
+
               </p>
               <div className="space-y-4">
                 <div>
                   <h3 className="text-xl font-semibold">Address</h3>
-                  <p className="text-gray-600">123 Fashion Ave, Suite 100 New York, NY 10001</p>
+                  <p className="text-gray-600">Pratulya Fashions Mumbai, Maharashtra, India</p>
                 </div>
                 <div>
                   <h3 className="text-xl font-semibold">Phone</h3>
-                  <p className="text-gray-600">+1 (555) 123-4567</p>
+                  <p className="text-gray-600">+91 ----------</p>
                 </div>
                 <div>
                   <h3 className="text-xl font-semibold">Email</h3>
-                  <p className="text-gray-600">contact@pratulya.com</p>
+                  <p className="text-gray-600">info@pratulyafashions.com</p>
                 </div>
               </div>
             </div>
 
-            {/* Right Column - Contact Form */}
-            <div className="lg:w-1/2 bg-white p-6 rounded-lg shadow-lg">
-              <h2 className="text-3xl font-bold mb-6">Get in Touch</h2>
+            <div className="lg:w-1/2 bg-white p-6 rounded-lg shadow-xl">
+              <h1 className="text-3xl font-bold mb-6">Get in Touch</h1>
               <form className="space-y-4">
                 <div>
                   <label className="block text-gray-700">Name</label>
                   <input
                     type="text"
                     placeholder="Your Name"
-                    className="mt-1 block w-full p-3 border border-black rounded-lg focus:outline-none focus:ring-2 focus:ring-[#d48701]"
+                    className="mt-1 block w-full p-3 border border-solid border-black/40 rounded-full focus:outline-none focus:ring-2 focus:ring-[#d48701]"
                   />
                 </div>
                 <div>
@@ -53,7 +53,7 @@ const Contact = () => {
                   <input
                     type="email"
                     placeholder="Your Email"
-                    className="mt-1 block w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#d48701]"
+                    className="mt-1 block w-full p-3 border border-solid border-black/40 rounded-full focus:outline-none focus:ring-2 focus:ring-[#d48701]"
                   />
                 </div>
                 <div>
@@ -62,12 +62,12 @@ const Contact = () => {
                     rows={1}
                     cols={30}
                     placeholder="Your Message"
-                    className="mt-1 block w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#d48701] h-32"
+                    className="mt-1 block w-full p-3 border border-solid border-black/40 rounded-xl focus:outline-none focus:ring-2 focus:ring-[#d48701] h-32"
                   ></textarea>
                 </div>
                 <button
                   type="submit"
-                  className="w-full bg-[#d48701] text-white p-3 rounded-lg font-semibold hover:bg-[#d48701] transition duration-200"
+                  className="w-full bg-[#d48701] text-white p-3 rounded-full font-semibold hover:bg-[#d48701] transition duration-200"
                 >
                   Send Message
                 </button>
